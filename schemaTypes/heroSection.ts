c5074f5bf25543e72e7563cf442c6d805828eccd
ChaudharyRitesh <ritesh.chaudhari.t5a17@icp.edu.np>
2025-12-31 14:08:43 +0530

    tag

--- schemaTypes/heroSection.ts
+++ schemaTypes/heroSection.ts
@@ -6,6 +6,12 @@ export default defineType({
   type: 'document',
   fields: [
     defineField({
+      name: 'tagline',
+      title: 'Tagline',
+      type: 'string',
+      validation: Rule => Rule.required()
+    }),
+    defineField({
       name: 'title',
       title: 'Main Title',
       type: 'string',
